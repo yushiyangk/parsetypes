95e0091f698efcc58d42246364bff247bb395c0d
Yu Shiyang <yu.shiyang@gnayihs.uy>
2023-05-27 01:37:36 +0100

    Added tox and generated docs

--- src/parsetypes/__init__.py
+++ src/parsetypes/__init__.py
@@ -1,7 +1,7 @@
 """
 	This package provides tools for parsing serialised data to recover their original underlying types.
 
-    The `TypeParser` class provides configurable type inference and parsing. The `reduce_types` function can be used to obtain consistent types for tabular data.
+    The `TypeParser` class provides configurable type inference and parsing. The `reduce_types()` function can be used to obtain consistent types for tabular data.
 """
 
 
@@ -10,3 +10,5 @@ __version__ = "0.1dev"
 from ._common import GenericValue, Nullable, Scalar, Value, ValueType
 from ._parser import TypeParser
 from ._reduce_types import reduce_types
+
+__all__ = ('TypeParser', 'reduce_types')
