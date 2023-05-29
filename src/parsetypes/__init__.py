fa3e809b0798de18ab6abc45f94192a0833cbc96
Yu Shiyang <yu.shiyang@gnayihs.uy>
2023-05-29 05:16:33 +0100

    Bumped patch version number and updated changelog

--- src/parsetypes/__init__.py
+++ src/parsetypes/__init__.py
@@ -8,7 +8,7 @@
 """
 
 
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 
 from ._common import AnyScalar, AnyScalarType, AnyValue, AnyValueType, GenericValue, Nullable
 from ._parser import TypeParser
